477103f7809920845cda64857395b711e8f29b2c
aalonso <alexander.alonso.marin@gmail.com>
2020-02-12 11:59:57 +0100

    added if that checks if the motor has an encoder before creating a model with encoder.

--- src/sardana/taurus/qt/qtgui/extra_pool/poolmotor.py
+++ src/sardana/taurus/qt/qtgui/extra_pool/poolmotor.py
@@ -1099,7 +1099,9 @@ class PoolMotorTVReadWidget(TaurusWidget):
             return
         TaurusWidget.setModel(self, model + '/Position')
         self.lbl_read.setModel(model + '/Position')
-        self.lbl_enc_read.setModel(model + '/Encoder')
+        if self.taurusValueBuddy().motor_dev is not None and \
+                self.taurusValueBuddy().hasEncoder():
+            self.lbl_enc_read.setModel(model + '/Encoder')
         # Handle User/Expert view
         self.setExpertView(self.taurusValueBuddy()._expertView)
         self.taurusValueBuddy().expertViewChanged.connect(
